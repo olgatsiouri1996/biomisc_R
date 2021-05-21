0756312be5c6a59f7e7579ba1c331f8fae485cc8
Olga Tsiouri <olgatsiouri@outlook.com>
2021-05-21 03:26:17 +0300

    Update trim_pdb.R

--- pdb_corner/trim_pdb.R
+++ pdb_corner/trim_pdb.R
@@ -2,7 +2,7 @@
 library(argparse)
 library(bio3d)
 # input parameters
-parser <- ArgumentParser(description='trim an input pdb')
+parser <- ArgumentParser(description='trim an input pdb file')
 parser$add_argument('--pdb', help= 'input pdb file')
 parser$add_argument('--chain', help= 'chain to select from pdb file')
 parser$add_argument('--start', type="integer", help= 'number of resudue to start')
@@ -11,7 +11,9 @@ parser$add_argument('--out', help= 'output pdb file')
 args <- parser$parse_args()
 # main
 input_pdb <- read.pdb(args$pdb)
+# select chain and residues
 idx <- atom.select(input_pdb, chain= args$chain, resno=args$start:args$end)
 trimed_pdb <- trim.pdb(input_pdb,inds = idx)
+# export as pdb
 write.pdb(trimed_pdb,file = args$out,type = c("ATOM"))
 
