2df04c040c99a37d5c0faa3f7d5ad64174ad6dbb
Olga Tsiouri <olgatsiouri@outlook.com>
2021-06-07 04:17:42 +0300

    Update trim_pdb_by_chain.R

--- pdb_corner/trim_pdb_by_chain.R
+++ pdb_corner/trim_pdb_by_chain.R
@@ -8,8 +8,10 @@ parser$add_argument('--chain', help= 'chain to select from pdb file')
 parser$add_argument('--out', help= 'output pdb file')
 args <- parser$parse_args()
 # main
+# inport pdb to R and select a chain
 input_pdb <- read.pdb(args$pdb)
 idx <- atom.select(input_pdb, chain= args$chain)
 trimed_pdb <- trim.pdb(input_pdb,inds = idx)
+# export as pdb
 write.pdb(trimed_pdb,file = args$out,type = c("ATOM"))
 
