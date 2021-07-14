b44a9fa8115f09f187ea511ae37ac7b86174351a
Olga Tsiouri <olgatsiouri@outlook.com>
2021-07-14 20:32:53 +0300

    add reverse complement sequence and coordinates

--- CpG_island_identification/CpG_island_identificator.R
+++ CpG_island_identification/CpG_island_identificator.R
@@ -3,8 +3,9 @@ library(argparse)
 library(ape)
 library(phylotools)
 library(stringr)
+library(Biostrings)
 # input parameters
-parser <- ArgumentParser(description='retrieve the start and end coordinates of CpG islands in a sequence, with their %GC content and Obs/Exp ratio, by using a sliding window aproach (Gardiner-Garden and Frommer, 1987)')
+parser <- ArgumentParser(description='retrieve the start and end coordinates of CpG islands in a sequence(forward and reverse complement), with their %GC content and Obs/Exp ratio, by using a sliding window aproach (Gardiner-Garden and Frommer, 1987)')
 parser$add_argument('--fasta', help= 'input fasta file')
 parser$add_argument('--win', type="integer", help= 'window size for the calculation of Obs/Exp ratio (integer)')
 parser$add_argument('--step', type="integer", help= 'step size for the calculation of Obs/Exp ratio (integer)')
@@ -15,8 +16,9 @@ args <- parser$parse_args()
 # main
 # import fasta as data frame
 imported_fasta <- read.fasta(file = args$fasta)
-# collect fasta sequence, length, window and step size from input parameters
+# collect fasta sequence, reverse complement sequence, length, window and step size from input parameters
 fasta_sequence <- as.character(imported_fasta$seq.text)
+rev_sequence <- paste(reverseComplement(DNAString(fasta_sequence)))
 seq_length <- nchar(as.character(imported_fasta$seq.text))
 window <- args$win
 step <- args$step
@@ -34,12 +36,22 @@ gc_content <- function(x) {
 # calculate indices where each substring will start
 starts <- seq(1, seq_length - window +1 ,by = step)
 # chop it up
-df <- data.frame(sapply(starts, function(i) {
+df1 <- data.frame(sapply(starts, function(i) {
   substr(fasta_sequence, i, i + window -1)
 }))
-# add begin, end, gc_content and gc_ratio columns
-df$start <- starts
-df$end <- nchar(as.character(df$sapply.starts..function.i...)) + starts -1
+# now do for the reverse complement sequence
+df2 <- data.frame(sapply(starts, function(i) {
+  substr(rev_sequence, i, i + window -1)
+}))
+# add begin, end columns
+# forward sequence
+df1$start <- starts
+df1$end <- nchar(as.character(df1$sapply.starts..function.i...)) + starts -1
+# reverse complement sequence
+df2$start <- starts - seq_length
+df2$end <- nchar(as.character(df2$sapply.starts..function.i...)) + starts -1 - seq_length
+# combine results and calculate the Obs/Exp ratio and %GC content
+df <-rbind(df1,df2)
 df$gc_content <- gc_content(df$sapply.starts..function.i...)
 df$gc_ratio <- gc_ratio(df$sapply.starts..function.i...)
 # fillter by Obs/Exp ratio and %GC content
